66808efe793280f8f90505d1fca57787e998ca57
Michael Skaug <michaelskaug@Michael-Skaugs-MacBook-Pro.local>
2016-08-28 14:04:16 -0600

    initial green cube example in three.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: { path: path.resolve(__dirname, "dist"),
+            publicPath: "/dist/",
+            filename: 'bundle.js' },
+
+  debug: true,
+  devtool: 'eval-source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+
+};
