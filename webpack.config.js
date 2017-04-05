b19f4472cdb9bb2981f2ebdd7be55fa58a65cfc8
Mike Skaug <mike.skaug@gmail.com>
2017-04-04 22:48:21 -0600

    update file-loader and reference images by relative path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: { path: path.resolve(__dirname, "dist"),
-            publicPath: "/dist/",
+            publicPath: "./dist/",
             filename: 'bundle.js' },
 
   resolve: {
